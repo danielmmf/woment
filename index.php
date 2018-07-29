0d1d26078fbe8daa421276a959ac83496bacbe19
Danielle Santana <daniellesantanaf@gmail.com>
2018-07-29 03:15:55 -0300

    Retirada 1

--- index.php
+++ index.php
@@ -1,18 +1,9 @@
 <!DOCTYPE HTML>
+<html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>WoMent</title>
+    <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
     <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0" />
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/splash/splash-icon.png">
-    <link rel="apple-touch-startup-image" href="images/splash/splash-screen.png" media="screen and (max-device-width: 320px)" />
-    <link rel="apple-touch-startup-image" href="images/splash/splash-screen@2x.png" media="(max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2)" />
-    <link rel="apple-touch-startup-image" sizes="640x1096" href="images/splash/splash-screen@3x.png" />
-    <link rel="apple-touch-startup-image" sizes="1024x748" href="images/splash/splash-screen-ipad-landscape" media="screen and (min-device-width : 481px) and (max-device-width : 1024px) and (orientation : landscape)" />
-    <link rel="apple-touch-startup-image" sizes="768x1004" href="images/splash/splash-screen-ipad-portrait.png" media="screen and (min-device-width : 481px) and (max-device-width : 1024px) and (orientation : portrait)" />
-    <link rel="apple-touch-startup-image" sizes="1536x2008" href="images/splash/splash-screen-ipad-portrait-retina.png" media="(device-width: 768px)    and (orientation: portrait) and (-webkit-device-pixel-ratio: 2)" />
-    <link rel="apple-touch-startup-image" sizes="1496x2048" href="images/splash/splash-screen-ipad-landscape-retina.png" media="(device-width: 768px)   and (orientation: landscape)    and (-webkit-device-pixel-ratio: 2)" />
-    <title>Epsilon Mobile Framework - Version 2.0</title>
     <link href="styles/style.css" rel="stylesheet" type="text/css">
     <link href="styles/colors/default.css" rel="stylesheet" type="text/css" id="pagestyle">
     <link href="styles/framework.css" rel="stylesheet" type="text/css">
@@ -37,14 +28,6 @@
     </script>
 </head>
 <body>
-    <div id="preloader">
-        <div id="status">
-            <p class="center-text">
-                Loading the content...
-                <em>Loading depends on your connection speed!</em>
-            </p>
-        </div>
-    </div>
     <div class="all-elements">
         <div id="sidebar" class="page-sidebar">
             <div class="page-sidebar-scroll">
@@ -53,93 +36,26 @@
                         <a href="#" class="sidebar-close"></a>
                         <em>Navigation</em>
                     </div>
-                    <div class="sidebar-decoration"></div>
-                    <a href="#" class="sidebar-logo"></a>
-                    <div class="sidebar-decoration"></div>
                     <div class="sidebar-navigation">
                         <div class="nav-item">
-                            <a href="#" class="dropdown-nav">Sidebar Styles</a>
-                            <div class="nav-item-submenu">
-                                <a href="#" onclick="swapStyleSheet('styles/colors/imagetwo.css')">Image Two</a>
-                                <a href="#" onclick="swapStyleSheet('styles/colors/imagethree.css')">Image Three</a>
-                                <a href="#" onclick="swapStyleSheet('styles/colors/imagefour.css')">Image Four</a>
-                                <a href="#" onclick="swapStyleSheet('styles/colors/imagefive.css')">Image Five</a>
-                                <a href="#" onclick="swapStyleSheet('styles/colors/blue.css')">Solid Blue</a>
-                                <a href="#" onclick="swapStyleSheet('styles/colors/green.css')">Solid Green</a>
-                                <a href="#" onclick="swapStyleSheet('styles/colors/orange.css')">Solid Orange</a>
-                                <a href="#" onclick="swapStyleSheet('styles/colors/red.css')">Solid Red</a>
-                                <a href="#" onclick="swapStyleSheet('styles/colors/black.css')">Solid Dark</a>
-                                <a href="#" onclick="swapStyleSheet('styles/colors/default.css')">Default Image</a>
-                            </div>
-                        </div>
-                        <div class="nav-item">
-                            <a href="index.html" class="active-nav">Homepage</a>
-                        </div>
-                        <div class="nav-item">
-                            <a href="#" class="dropdown-nav">Features</a>
-                            <div class="nav-item-submenu">
-                                <a href="intro.html">Introduction</a>
-                                <a href="type.html">Typography</a>
-                                <a href="jquery.html">jQuery</a>
-                            </div>
-                        </div>
-                        <div class="nav-item">
-                            <a href="#" class="dropdown-nav">Gallery</a>
-                            <div class="nav-item-submenu">
-                                <a href="wide.html">Wide Portfolio</a>
-                                <a href="onefolio.html">One Column</a>
-                                <a href="twofolio.html">Two Columns</a>
-                                <a href="threefolio.html">Three Columns</a>
-                                <a href="thumbfolio.html">Thumbnail Gallery</a>
-                                <a href="videos.html">Responsive Videos</a>
-                            </div>
+                            <a href="index.php" class="active-nav">Home</a>
                         </div>
                         <div class="nav-item">
                             <a href="contact.html" class="nav-regular">Contact</a>
                         </div>
-                        <div class="nav-item">
-                            <a href="#" class="nav-regular close-sidebar">Close</a>
-                        </div>
                     </div>
-                    <div class="sidebar-decoration"></div>
-                    <div class="sidebar-section">
-                        <a href="#" class="sidebar-social"></a>
-                        <em>Get in touch</em>
-                    </div>
-                    <div class="sidebar-decoration"></div>
-                    <div class="sidebar-navigation">
-                        <div class="nav-item">
-                            <a href="#" class="call-nav">Call us</a>
-                        </div>
-                        <div class="nav-item">
-                            <a href="#" class="text-nav">Text us</a>
-                        </div>
-                        <div class="nav-item">
-                            <a href="#" class="link-nav">Facebook</a>
-                        </div>
-                        <div class="nav-item">
-                            <a href="#" class="link-nav">Twitter</a>
-                        </div>
-                        <div class="nav-item">
-                            <a href="#" class="link-nav">Google +</a>
-                        </div>
-                    </div>
-                    <div class="sidebar-decoration"></div>
-                    <p class="sidebar-copyright">Copyright 2013. All rights reserved</p>
                 </div>
             </div>
         </div>
         <div id="content" class="page-content">
             <div class="content-controls">
                 <a href="#" class="deploy-sidebar"></a>
-                <em>HOMEPAGE</em>
+                <em>HOME</em>
             </div>
             <div class="content">
                 <div class="decoration"></div>
                 <div class="content-header">
                     <a href="#" class="content-logo"></a>
-                    <a href="#" class="content-twitter"></a>
-                    <a href="#" class="content-facebook"></a>
                 </div>
                 <div class="container">
                     <div class="slider-controls" data-snap-ignore="true">
@@ -176,15 +92,13 @@
                     <p>Lorem ipsum dolor sit amet, consectetur adipisc ing elit. In adipiscing sem vel lorem elementum.</p>
                 </div>
                 <div class="container">
-                    <a href="#" class="next-quote"></a>
-                    <a href="#" class="prev-quote"></a>
                     <div class="quote-slider" data-snap-ignore="true">
                         <div>
                             <div class="services-item">
-                                <h4>Elite Author</h4>
-                                <div class="overlay dark-overlay"></div>
+                                <h4>HTML</h4>
+                                <div class="overlay yellow-overlay"></div>
                                 <img src="images/general-nature/3s.jpg" alt="img">
-                                <strong>We love quotes, and sometimes it's annoying to see tons of them that you need to scroll to!</strong>
+                                <strong>Short loin beef tongue, fatback bacon doner swine. Andouille sausage shoulder pork biltong</strong>
                                 <a href="#" class="button button-dark center-button">Button One</a>
                             </div>
                         </div>
@@ -193,71 +107,27 @@
                                 <h4>Mobile Web</h4>
                                 <div class="overlay red-overlay"></div>
                                 <img src="images/general-nature/3s.jpg" alt="img">
-                                <strong>We love quotes, and sometimes it's annoying to see tons of them that you need to scroll to!</strong>
+                                <strong>Short loin beef tongue, fatback bacon doner swine. Andouille sausage shoulder pork biltong</strong>
                                 <a href="#" class="button button-red center-button">Button One</a>
                             </div>
                         </div>
                         <div>
                             <div class="services-item">
-                                <h4>Mobile Web</h4>
+                                <h4>Gestao de tempo</h4>
                                 <div class="overlay blue-overlay"></div>
                                 <img src="images/general-nature/4s.jpg" alt="img">
-                                <strong>We love quotes, and sometimes it's annoying to see tons of them that you need to scroll to!</strong>
+                                <strong>Short loin beef tongue, fatback bacon doner swine. Andouille sausage shoulder pork biltong</strong>
                                 <a href="#" class="button button-blue center-button">Button Two</a>
-                            </div>
-                        </div>
-                        <div>
-                            <div class="services-item">
-                                <h4>CSS3 Code</h4>
-                                <div class="overlay yellow-overlay"></div>
-                                <img src="images/general-nature/5s.jpg" alt="img">
-                                <strong>We love quotes, and sometimes it's annoying to see tons of them that you need to scroll to!</strong>
-                                <a href="#" class="button button-yellow center-button">Button Three</a>
-                            </div>
-                        </div>
-                        <div>
-                            <div class="services-item">
-                                <h4>PSD Included</h4>
-                                <div class="overlay green-overlay"></div>
-                                <img src="images/general-nature/6s.jpg" alt="img">
-                                <strong>We love quotes, and sometimes it's annoying to see tons of them that you need to scroll to!</strong>
-                                <a href="#" class="button button-green center-button">Button Four</a>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="decoration"></div>
                 <div class="container no-bottom">
-                    <h4>Show off your works!</h4>
-                    <p>In an awesome thumbnail gallery that's made to shine and works flawlessly with swipe ability too!</p>
-                </div>
-                <div class="container no-bottom">
-                    <ul class="gallery round-thumb">
-                        <li>
-                            <a class="swipebox" href="images/gallery/full/1.jpg" title="An image caption!">
-                            <img src="images/general-nature/1s.jpg" alt="img" /></a>
-                        </li>
-                        <li>
-                            <a class="swipebox" href="images/gallery/full/2.jpg" title="It can change!">
-                            <img src="images/general-nature/2s.jpg" alt="img" /></a>
-                        </li>
-                        <li>
-                            <a class="swipebox" href="images/gallery/full/3.jpg" title="To be whatever you want!">
-                            <img src="images/general-nature/3s.jpg" alt="img" /></a>
-                        </li>
-                        <li>
-                            <a class="swipebox" href="images/gallery/full/1.jpg" title="It's connected to the href!">
-                            <img src="images/general-nature/4s.jpg" alt="img" /></a>
-                        </li>
-                        <li>
-                            <a class="swipebox" href="images/gallery/full/2.jpg" title="Easy to change and edit!">
-                            <img src="images/general-nature/5s.jpg" alt="img" /></a>
-                        </li>
-                        <li>
-                            <a class="swipebox" href="images/gallery/full/3.jpg" title="What an awesome gallery!">
-                            <img src="images/general-nature/6s.jpg" alt="img" /></a>
-                        </li>
-                    </ul>
+                    <h4>BACON</h4>
+                    <p>Short loin beef tongue, fatback bacon doner swine. Andouille sausage shoulder pork biltong
+                    Short loin beef tongue, fatback bacon doner swine. Andouille sausage shoulder pork biltong
+                    Short loin beef tongue, fatback bacon doner swine. Andouille sausage shoulder pork biltongShort loin beef tongue, fatback bacon doner swine. Andouille sausage shoulder pork biltongShort loin beef tongue, fatback bacon doner swine. Andouille sausage shoulder pork biltongShort loin beef tongue, fatback bacon doner swine. Andouille sausage shoulder pork biltong</p>
                 </div>
             </div>
         </div>
